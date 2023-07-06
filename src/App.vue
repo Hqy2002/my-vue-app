ba19ae43a487d591f35afab2c2ee8155f9b733ba
HQY <1311026322@qq.com>
2023-07-06 16:09:05 +0800

    modify the login vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import Login from './components/pages/Login.vue'
+import Register from "./components/pages/Register.vue";
 </script>
 
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="/vite.svg" className="logo" alt="Vite logo"/>
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="./assets/vue.svg" className="logo vue" alt="Vue logo"/>
     </a>
   </div>
 
   <Login>'msg='hello'</Login>
+  <Register>'msg='hello'</Register>
+
 </template>
 
 <style scoped>
@@ -23,9 +26,11 @@ import Login from './components/pages/Login.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
