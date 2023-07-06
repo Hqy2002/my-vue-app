2b048f0d0e416f83099b63a0eca5138ce3cb5148
HQY <1311026322@qq.com>
2023-07-06 09:37:30 +0800

    Initial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    用户名:<input type="text" v-model="loginForm.username" placeholder="请输入用户名"/>
+    <br><br>
+    密码： <input type="password" v-model="loginForm.password" placeholder="请输入密码"/>
+    <br><br>
+    <button v-on:click="login">登录</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
